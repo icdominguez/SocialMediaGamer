d5a42444d3835d02cbe641b61a6602acc2dceb6f
icordondominguez <icordondominguez@gmail.com>
2021-08-11 09:27:33 +0200

    Fixed error on package name and modified user class and logic

--- app/src/main/java/com/icdominguez/socialmediagamer/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.icdominguez.socialmediagamer.model
-
-data class User (
-    var email: String? = null,
-    var username: String? = null,
-    var phone: String? = null,
-    var password: String? = null
-)
--- app/src/main/java/com/icdominguez/socialmediagamerkotlin/common/Constants.kt
+++ app/src/main/java/com/icdominguez/socialmediagamerkotlin/common/Constants.kt
@@ -1,4 +1,4 @@
-package com.icdominguez.socialmediagamer.common
+package com.icdominguez.socialmediagamerkotlin.common
 
 object Constants {
     val USER_CREATED = "UserCreated"
