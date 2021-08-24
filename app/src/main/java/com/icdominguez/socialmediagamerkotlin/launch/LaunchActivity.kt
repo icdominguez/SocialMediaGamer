cd177bf683a4faaff1b0e2531050da8e2b5cb457
icordondominguez <icordondominguez@gmail.com>
2021-08-24 08:19:24 +0200

    SplashScreen finished

--- app/src/main/java/com/icdominguez/socialmediagamerkotlin/launch/LaunchActivity.kt
+++ app/src/main/java/com/icdominguez/socialmediagamerkotlin/launch/LaunchActivity.kt
@@ -9,37 +9,21 @@ import com.icdominguez.socialmediagamerkotlin.databinding.ActivityLaunchBinding
 import com.icdominguez.socialmediagamerkotlin.home.HomeRouter
 import com.icdominguez.socialmediagamerkotlin.login.LoginRouter
 import com.icdominguez.socialmediagamerkotlin.login.LoginViewModel
+import com.icdominguez.socialmediagamerkotlin.provider.AuthProvider
 import com.shashank.sony.fancytoastlib.FancyToast
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class LaunchActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLaunchBinding
-    private lateinit var viewModel: LoginViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        setTheme(R.style.AppTheme)
-
         super.onCreate(savedInstanceState)
-        binding = ActivityLaunchBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-
-
-        GlobalScope.launch {
-            checkUser()
-        }
-
-    }
-
-    suspend fun checkUser() {
-        if(viewModel.checkUser()) {
+        if(AuthProvider().checkUser() != null) {
             HomeRouter().launch(applicationContext)
         } else {
             LoginRouter().launch(applicationContext)
         }
     }
+
 }
